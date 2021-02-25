5f34e1f030f37bcba8c4bcdb22bb3b0a1c320b09
Jess Chin <jmalvinchin@gmail.com>
2021-02-25 15:13:59 +0800

    Add global filter from react-table

--- src/components/common/Table.js
+++ src/components/common/Table.js
@@ -1,12 +1,13 @@
-import React from "react"
-import { useTable, useSortBy } from "react-table"
+import React, { Fragment } from "react"
+import { useTable, useSortBy, useGlobalFilter, useAsyncDebounce } from "react-table"
 
 function Table({columns, data}) {
   const tableInstance = useTable({
     columns,
     data
   },
-    useSortBy
+    useGlobalFilter,
+    useSortBy,
   )
   const {
     getTableProps,
@@ -14,37 +15,77 @@ function Table({columns, data}) {
     headerGroups,
     rows,
     prepareRow,
+    state,
+    preGlobalFilteredRows,
+    setGlobalFilter,
   } = tableInstance
 
   return(
-    <table {...getTableProps()} className="w-full border-collapse">
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()} className="p-4">
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps(column.getSortByToggleProps())} className="border text-center m-4">
-                {column.render("Header")}
-                <span>
-                  {column.isSorted ? (column.isSortedDesc ? " 🔽" : " 🔼") : ""}
-                </span>
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()} className="border text-center">{cell.render("Cell")}</td>;
-              })}
+    <Fragment>
+      <GlobalFilter
+        preGlobalFilteredRows={preGlobalFilteredRows}
+        globalFilter={state.globalFilter}
+        setGlobalFilter={setGlobalFilter}
+      />
+      <table {...getTableProps()} className="w-full border-collapse">
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()} className="p-4">
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())} className="border text-center m-4">
+                  {column.render("Header")}
+                  <span>
+                    {column.isSorted ? (column.isSortedDesc ? " 🔽" : " 🔼") : ""}
+                  </span>
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return <td {...cell.getCellProps()} className="border text-center">{cell.render("Cell")}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </Fragment>
+  )
+}
+
+function GlobalFilter({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}) {
+  const count = preGlobalFilteredRows.length
+  const [value, setValue] = React.useState(globalFilter)
+  const onChange = useAsyncDebounce(value => {
+    setGlobalFilter(value || undefined)
+  }, 200)
+
+  return (
+    <span>
+      Search:{' '}
+      <input
+        value={value || ""}
+        onChange={e => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+        placeholder={`${count} records...`}
+        style={{
+          fontSize: '1.1rem',
+          border: '0',
+        }}
+      />
+    </span>
   )
 }
 
